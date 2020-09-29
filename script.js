f8f251a5738cfa9d11f318595d9cd1363f21a72c
programmers42p <democracy444@mail.ru>
2020-09-29 17:11:23 +0300

    num inf

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let num0 = prompt();
 
-if(num > 0){
+if(num0 > 0){
   console.log("Число больше нуля")
-} else if (num < 0){
+} else if (num0 < 0){
   console.log("Число меньше нуля")
 }else{
    console.log("0")
 }
-if (num % 2 ==0){
+if (num0 % 2 ==0){
    console.log("Чётное")
 
 }else{
