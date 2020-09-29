6eb51d5ef6c4a4e98cc1aa614a73e5993aeb8aa3
programmers42p <71330100+programmers42p@users.noreply.github.com>
2020-09-29 19:49:15 +0300

    Function

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,15 +1,19 @@
 let num0 = prompt();
 
-if(num0 > 0){
-  console.log("Число больше нуля")
-} else if (num0 < 0){
-  console.log("Число меньше нуля")
-}else{
-   console.log("0")
-}
-if (num0 % 2 ==0){
-   console.log("Чётное")
+function numberinfo(n1){
+    if(num0 > 0){
+      console.log("Число больше нуля")
+    } else if (num0 < 0){
+      console.log("Число меньше нуля")
+    }else{
+       console.log("0")
+    }
+    if (num0 % 2 ==0){
+       console.log("Чётное")
 
-}else{
-   console.log("Нечётное")
+    }else{
+       console.log("Нечётное")
+    }
 }
+console.log(numberinfo(num0))
+
