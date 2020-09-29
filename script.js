fea2f31892b513bcc582828a103e6c4cee7435ca
programmers42p <71330100+programmers42p@users.noreply.github.com>
2020-09-29 17:16:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let num0 = prompt();
-let num1 = prompt();
 
-let origin = num0;
-function sqrnum(n0, n1){
-  for(i = 1; i < num1; i++){
-    num0 *= origin;
-  }
-  return num0;
+if(num > 0){
+  console.log("Число больше нуля")
+} else if (num < 0){
+  console.log("Число меньше нуля")
+}else{
+   console.log("0")
 }
-alert(sqrnum(num0,num1));
+if (num % 2 ==0){
+   console.log("Чётное")
+
+}else{
+   console.log("Нечётное")
+}
